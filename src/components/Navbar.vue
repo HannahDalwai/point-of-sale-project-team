d88b67a16b636efb9cada33a13a552b45ca75f28
Hannah Dalwai <hannahdalwai4@gmail.com>
2022-02-25 16:17:04 +0200

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
  
-    <nav class="navbar navbar-expand navbar-light" style="background-color: #473c75; ">
+    <!-- <nav class="navbar navbar-expand navbar-light" style="background-color: #473c75; ">
       <router-link :to="{name:'home'}" class="navbar-brand">Mr. Magorium's Wonder Emporium</router-link>
       <div class="navbar-nav mr-auto">
         <li class="nav-item">
@@ -46,6 +46,63 @@
       </div>
     </nav>
     
+ -->
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+   <router-link :to="{name:'home'}" class="navbar-brand">Mr. Magorium's Wonder Emporium</router-link>
+      <div class="navbar-nav mr-auto">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link :to="{name:'home'}" class="nav-link">
+            <font-awesome-icon icon="home" /> Home
+          </router-link>
+        </li>
+        <li class="nav-item">
+           <router-link :to="{name :'register'}" class="nav-link">
+            <font-awesome-icon icon="user-plus" /> Sign Up
+          </router-link>
+        </li>
+        <li class="nav-item">
+        <router-link :to="{name:'login'}" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt" /> Login
+          </router-link>
+        </li>
+      </ul>
+       </div>
+        <div v-if="currentUser" class="navbar-nav ml-auto">
+          <li class="nav-item">
+          <router-link :to="{name:'profile'}" class="nav-link">
+            <font-awesome-icon icon="user" />
+            {{ currentUser.username }}
+          </router-link>
+        </li>
+          <li class="nav-item">
+          <router-link :to="{name:'cart'}" class="nav-link">
+            <font-awesome-icon icon="sign-in-alt" /> Cart
+          </router-link>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" @click.prevent="logOut">
+            <font-awesome-icon icon="sign-out-alt" /> LogOut
+          </a>
+        </li>
+        </div>
+    </div>
+  </div>
+</nav>
+
+
+
+
 </template>
 
 <script>
